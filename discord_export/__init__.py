3bbe81d5dac8cc617bb7615cc7e032fe82b57b48
Oleg T <rapatka@yahoo.com>
2021-02-11 07:33:42 +0400

    add dynamic_ncols=True to progress bar

--- discord_export/__init__.py
+++ discord_export/__init__.py
@@ -94,7 +94,7 @@ async def export_channel(discord, args):
 
         exported = 0
         limit = 100
-        with tqdm.tqdm(total=100, bar_format='{desc} [{bar}] {percentage:3.2f}%') as pbar:
+        with tqdm.tqdm(total=100, dynamic_ncols=True, bar_format='{desc} [{bar}] {percentage:3.2f}%') as pbar:
             while True:
                 messages = await d.get(f'/channels/{args.channel_id}/messages', after=after, limit=limit)
                 messages = list(reversed(messages))
